22cf8cd297ee9e3c42d298a7dfb1aeb7f46c4dcc
Nelson Onuegbu <leonenelson38@gmail.com>
2026-04-04 20:52:41 +0100

    Add favicon, manifest, and update assets
    - Added favicon.ico and favicon.png to enhance site branding.
    - Created site.webmanifest for improved PWA support and icon management.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,13 @@ export default async function Layout({ children }: { children: ReactNode }) {
 
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+                <link rel="icon" href="/favicon.png" type="image/png" />
+                <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+                <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+            </head>
             <body className={`${inter.className} min-h-screen`}>
                 <ReduxStoreProvider>
                     <div className="flex justify-end min-h-screen bg-gray-100/80">
